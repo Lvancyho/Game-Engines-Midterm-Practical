b42f2bd79439713b0c97ff1059b0d75856f3a6c6
Lvancyho <ivanhocy@gmail.com>
2024-10-08 09:01:02 -0400

    Created player and enemy

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This is my singleton, it's a singleton because there can only be one game manager at all times 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
